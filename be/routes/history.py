860b60c269d4ed93833f7b32c564baab0ab51c55
AanviKothari456 <kothariaanvi@gmail.com>
2025-05-27 14:42:54 +0530

    Commit 4: Add history blueprint stub

--- be/routes/history.py
+++ be/routes/history.py
@@ -1,9 +1,1 @@
-# routes/history.py
-from flask import Blueprint, jsonify
-from services.history_store import get_history
-
-history_bp = Blueprint("history", __name__)
-
-@history_bp.route("/api/history", methods=["GET"])
-def history():
-    return jsonify({"history": get_history()})
+from flask import Blueprint\nhistory_bp = Blueprint('history', __name__)
