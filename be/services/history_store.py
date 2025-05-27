f2c10ad09392403d1f5dfe2568940f60d87909e7
AanviKothari456 <kothariaanvi@gmail.com>
2025-05-27 14:42:55 +0530

    Commit 8: Add save_summary logic

--- be/services/history_store.py
+++ be/services/history_store.py
@@ -1,9 +1,1 @@
-# services/history_store.py
-
-history_log = []
-
-def save_summary(url, summary):
-    history_log.append({"url": url, "summary": summary})
-
-def get_history():
-    return history_log[::-1]  # return most recent first
+history_log = []\ndef save_summary(u,s): history_log.append({'url':u,'summary':s})
