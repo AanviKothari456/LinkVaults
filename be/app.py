1291a5a4f7b818e295e0c27e1ae3ab4138a710f2
AanviKothari456 <kothariaanvi@gmail.com>
2025-05-27 14:44:13 +0530

    Commit 2: Set up basic Flask app

--- be/app.py
+++ be/app.py
@@ -1,7 +1,1 @@
-from flask_cors import CORS
 from flask import Flask\napp = Flask(__name__)\n@app.route('/')\ndef home(): return 'OK'
-from routes.summarize import summarize_bp
-from routes.history import history_bp
-app.register_blueprint(history_bp)
-CORS(app)
-from dotenv import load_dotenv\nload_dotenv()
