6a402bcabf64c110e878f9bfa83e6989f417d832
AanviKothari456 <kothariaanvi@gmail.com>
2025-05-27 14:44:55 +0530

    Commit 16: Add CORS support

--- be/app.py
+++ be/app.py
@@ -1,4 +1,6 @@
+from flask_cors import CORS
 from flask import Flask\napp = Flask(__name__)\n@app.route('/')\ndef home(): return 'OK'
 from routes.summarize import summarize_bp
 from routes.history import history_bp
 app.register_blueprint(history_bp)
+CORS(app)
