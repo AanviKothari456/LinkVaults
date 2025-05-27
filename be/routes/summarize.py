ab8256d842bd3e815cf46b5e979aa9536f57a173
AanviKothari456 <kothariaanvi@gmail.com>
2025-05-27 14:42:55 +0530

    Commit 12: Add summarize API route stub

--- be/routes/summarize.py
+++ be/routes/summarize.py
@@ -1,3 +1,4 @@
 from flask import Blueprint\nsummarize_bp = Blueprint('summarize', __name__)
 from services.openai_summary import get_summary
 from services.history_store import save_summary
+@summarize_bp.route('/api/summarize', methods=['POST'])\ndef summarize(): return 'ok'
