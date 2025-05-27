ec3a60f5bc362e18f58828498487f4285b5c9cc6
AanviKothari456 <kothariaanvi@gmail.com>
2025-05-27 14:40:52 +0530

    updated hsitory for app.py

--- be/app.py
+++ be/app.py
@@ -2,12 +2,14 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.summarize import summarize_bp
+from routes.history import history_bp
+
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(summarize_bp)
-
+app.register_blueprint(history_bp)
 @app.route("/")
 def home():
     return "LinkVault API running"
