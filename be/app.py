962d8d96a3cd0c88bec677244df3060991dcd769
AanviKothari456 <kothariaanvi@gmail.com>
2025-05-27 14:42:54 +0530

    Commit 6: Register history blueprint

--- be/app.py
+++ be/app.py
@@ -1,2 +1,4 @@
 from flask import Flask\napp = Flask(__name__)\n@app.route('/')\ndef home(): return 'OK'
 from routes.summarize import summarize_bp
+from routes.history import history_bp
+app.register_blueprint(history_bp)
