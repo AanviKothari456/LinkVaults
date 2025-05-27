b4deaeba9765a97acb542ac75fcd88718132a8e3
AanviKothari456 <kothariaanvi@gmail.com>
2025-05-27 14:42:55 +0530

    Commit 14: Add simple URL validator

--- be/utils/validator.py
+++ be/utils/validator.py
@@ -1,9 +1,1 @@
-# utils/validator.py
-import re
-
-def is_valid_url(url):
-    pattern = re.compile(
-        r"^(http|https)://"
-        r"(\w+\.)+\w+(/\S*)?$"
-    )
-    return bool(pattern.match(url))
+def is_valid_url(u): return u.startswith('http')
